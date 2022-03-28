d6ccf13712ae4c5890714b12367e77ad9a323f45
mwangi julius <julius.mwangi@moringaschool.com>
2022-03-28 17:39:40 +0300

    commit tree aeaeb4895e13cc13951ed1bb99001243be7728ee

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,21 @@
 #write your code here
 
-def countdown
-    x = 0
-    while x < 10
-        puts "#{x} is less than 10"
-        x += 1
+def countdown(from = 10)
+    x = from
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
     end
+    "HAPPY NEW YEAR!"
 end
 
-countdown()
+def countdown_with_sleep (from = 10)
+    x = from
+    while x > 0
+        sleep(1)
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    sleep(1)
+    "HAPPY NEW YEAR!"
+end
